b80b64b54e512b4b69f2815212f8fb0ff6753375
Mike <kiltsarebreezy@hotmail.com>
2020-01-04 12:49:58 -0700

    Up Coverage

---
+++ src/Wiki.Tests/ArticleTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wiki.Tests
+{
+    [TestClass]
+    public class ArticleTests
+    {
+        [TestMethod]
+        public void SetClear()
+        {
+            // Arrange
+            var article = new Article();
+            var body = new Body { Markdown = "Boo" };
+            // Act
+            // Assert
+            Assert.AreEqual(0, article.Content.Count);
+            
+            article.Set(body);
+            
+            Assert.AreEqual(1, article.Content.Count);
+            Assert.IsInstanceOfType(article.Content[0], typeof(Body));
+            Assert.AreEqual("Boo", ((Body)article.Content[0]).Markdown);
+
+            article.Set<Body>(null);
+
+            Assert.AreEqual(0, article.Content.Count);
+        }
+    }
+}
